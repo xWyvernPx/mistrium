ec8e02f20dd26d1493867aa34e334523ae6b5c2d
xWyvernPx <phonglethanh2@gmail.com>
2022-09-15 19:13:37 +0700

    vite output build update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../server/src/web/public",
-    // outDir: "./dist",
+    // outDir: "../server/src/web/public",
+    outDir: "./dist",
   },
 });
